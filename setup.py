d7532760b6c1eb96beaa90bf948f409f36d0130c
João Magalhães <joamag@gmail.com>
2025-11-14 09:11:06 +0000

    feat: add test suite configuration to setup.py

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setuptools.setup(
     url="http://prismic-api.hive.pt",
     zip_safe=False,
     packages=["prismic"],
+    test_suite="prismic.test",
     package_dir={"": os.path.normpath("src")},
     install_requires=["appier"],
     classifiers=[
